cdf08ae2074e2e8d2c52d44a6e91bdc0343fe5dd
Jacek Czastkiewicz <external.jacek.czastkiewicz@kuehne-nagel.com>
2022-09-26 08:50:35 +0200

    Add tests for preparation out adapters

--- preparation/adapter-out-db/src/main/java/hexarch/dms/preparation/adapter/out/db/DocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package hexarch.dms.preparation.adapter.out.db;
-
-import hexarch.dms.preparation.domain.Document;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentRepository extends JpaRepository<Document, Long> {
-}
