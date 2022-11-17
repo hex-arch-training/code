41b621730fe2c038c19e8b53b078dffc7f282e6c
Jacek Czastkiewicz <external.jacek.czastkiewicz@kuehne-nagel.com>
2022-11-17 16:26:36 +0100

    Add test

--- verification/adapter-in-web/src/test/java/hexarch/dms/verification/adapter/in/web/RevisionVerificationRestControllerTest.java
+++ verification/adapter-in-web/src/test/java/hexarch/dms/verification/adapter/in/web/RevisionVerificationRestControllerTest.java
@@ -1,7 +1,11 @@
 package hexarch.dms.verification.adapter.in.web;
 
+import hexarch.dms.verification.application.port.RevisionPreviewQueryModel;
 import hexarch.dms.verification.application.port.in.AcceptRevisionCommand;
 import hexarch.dms.verification.application.port.in.AcceptRevisionUseCase;
+import hexarch.dms.verification.application.port.in.QueryRevisionPreviewUseCase;
+import hexarch.dms.verification.domain.DocumentRevisionId;
+import hexarch.dms.verification.domain.VerificationStatus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -11,11 +15,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,11 +38,17 @@ class RevisionVerificationRestControllerTest {
     @MockBean
     private AcceptRevisionUseCase acceptRevisionUseCase;
 
+    @MockBean
+    private QueryRevisionPreviewUseCase queryRevisionPreviewUseCase;
+
     @Autowired
     private MockMvc mvc;
 
     @Captor
     private ArgumentCaptor<AcceptRevisionCommand> acceptRevisionCommandCaptor;
+
+    @Captor
+    private ArgumentCaptor<DocumentRevisionId> documentRevisionIdCaptor;
 
     @Test
     void shouldCreateVerificationRequest() throws Exception {
@@ -48,5 +64,47 @@ class RevisionVerificationRestControllerTest {
                 .andExpect(status().isOk());
         verify(acceptRevisionUseCase, times(1)).apply(acceptRevisionCommandCaptor.capture());
         assertThat(acceptRevisionCommandCaptor.getValue().getRevisionId().getRevisionId()).isEqualTo(REVISION_ID);
+    }
+
+    @Test
+    void shouldReturnNoPreviewIfNotFound() throws Exception {
+        // given
+
+        // when
+        var resultActions = mvc.perform(get("/revision/{revisionId}/preview", REVISION_ID)
+                .contentType(APPLICATION_JSON_VALUE));
+
+        // then
+        resultActions
+                .andExpect(status().isNotFound());
+        verify(queryRevisionPreviewUseCase, times(1)).queryBy(documentRevisionIdCaptor.capture());
+        assertThat(documentRevisionIdCaptor.getValue().getRevisionId()).isEqualTo(REVISION_ID);
+    }
+
+    @Test
+    void shouldReturnPreview() throws Exception {
+        // given
+        String title = "title";
+        String content = "content";
+        var  documentRevisionId = new DocumentRevisionId(1);
+        VerificationStatus verificationStatus = VerificationStatus.PENDING;
+
+        var revisionPreviewQueryModel = Optional.of(
+                new RevisionPreviewQueryModel(title, content, documentRevisionId, verificationStatus));
+
+        when(queryRevisionPreviewUseCase.queryBy(any())).thenReturn(revisionPreviewQueryModel);
+
+        // when
+        var resultActions = mvc.perform(get("/revision/{revisionId}/preview", REVISION_ID)
+                .contentType(APPLICATION_JSON_VALUE));
+
+        // then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value(title))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content").value(content))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.verificationStatus").value(verificationStatus.toString()));
+        verify(queryRevisionPreviewUseCase, times(1)).queryBy(documentRevisionIdCaptor.capture());
+        assertThat(documentRevisionIdCaptor.getValue().getRevisionId()).isEqualTo(REVISION_ID);
     }
 }
---
+++ verification/application/src/test/java/hexarch/dms/verification/application/QueryRevisionPreviewServiceTest.java
@@ -0,0 +1,60 @@
+package hexarch.dms.verification.application;
+
+import hexarch.dms.verification.application.port.RevisionContentQueryModel;
+import hexarch.dms.verification.application.port.RevisionPreviewQueryModel;
+import hexarch.dms.verification.application.port.out.FindRevisionContentPort;
+import hexarch.dms.verification.application.port.out.FindRevisionVerificationPort;
+import hexarch.dms.verification.domain.DocumentRevisionId;
+import hexarch.dms.verification.domain.RevisionVerification;
+import hexarch.dms.verification.domain.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class QueryRevisionPreviewServiceTest {
+
+    private static final DocumentRevisionId DOCUMENT_REVISION_ID = new DocumentRevisionId(44L);
+
+    @Mock
+    private FindRevisionVerificationPort findRevisionVerificationPort;
+
+    @Mock
+    private FindRevisionContentPort findRevisionContentPort;
+
+    @InjectMocks
+    private QueryRevisionPreviewService queryRevisionPreviewService;
+
+    @Test
+    public void shouldFindRevisionPreview() {
+        // given
+        var user = new User("one");
+        var revisionVerification = RevisionVerification.createNew(DOCUMENT_REVISION_ID, user);
+        when(findRevisionVerificationPort.findByRevisionId(DOCUMENT_REVISION_ID)).thenReturn(Optional.of(revisionVerification));
+        var title = "title";
+        var content = "content";
+        var revisionContentQueryModel = new RevisionContentQueryModel(title, content);
+        when(findRevisionContentPort.queryBy(DOCUMENT_REVISION_ID)).thenReturn(Optional.of(revisionContentQueryModel));
+
+        // when
+        Optional<RevisionPreviewQueryModel> revisionPreviewQueryModel = queryRevisionPreviewService.queryBy(DOCUMENT_REVISION_ID);
+
+        // then
+        assertThat(revisionPreviewQueryModel)
+                .isPresent()
+                .hasValueSatisfying(value -> {
+                    assertThat(value.getDocumentRevisionId()).isEqualTo(revisionVerification.getDocumentRevisionId());
+                    assertThat(value.getVerificationStatus()).isEqualTo(revisionVerification.getVerificationStatus());
+                    assertThat(value.getTitle()).isEqualTo(revisionContentQueryModel.getTitle());
+                    assertThat(value.getContent()).isEqualTo(revisionContentQueryModel.getContent());
+                });
+
+    }
+}
