4c7f933f32d2ede60dcb9b0482335e663c1aa900
Jacek Czastkiewicz <external.jacek.czastkiewicz@kuehne-nagel.com>
2022-11-21 15:16:48 +0100

    Add ports

---
+++ verification/application/src/main/java/hexarch/dms/verification/application/port/in/QueryRevisionPreviewUseCase.java
@@ -0,0 +1,11 @@
+package hexarch.dms.verification.application.port.in;
+
+import hexarch.dms.verification.application.port.RevisionPreviewQueryModel;
+import hexarch.dms.verification.domain.DocumentRevisionId;
+
+import java.util.Optional;
+
+public interface QueryRevisionPreviewUseCase {
+
+    Optional<RevisionPreviewQueryModel> queryBy(DocumentRevisionId revisionId);
+}
