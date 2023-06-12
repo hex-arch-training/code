1a356f2a7028aacad15e4ea783e2664e78b5341a
Maciej Małecki <maciej.malecki@capgemini.com>
2023-06-12 20:47:24 +0200

    11 acceptRevision and queryRevisionVerification adapters web

--- verification/adapter-in-web/src/main/java/hexarch/dms/verification/adapter/in/web/RevisionVerificationExceptionHandler.java
+++ verification/adapter-in-web/src/main/java/hexarch/dms/verification/adapter/in/web/RevisionVerificationExceptionHandler.java
@@ -1,4 +1,26 @@
 package hexarch.dms.verification.adapter.in.web;
 
-class RevisionVerificationExceptionHandler {
+import hexarch.dms.verification.application.VerificationRequestNotFoundException;
+import hexarch.dms.verification.domain.VerificationStatusException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+class RevisionVerificationExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = {VerificationStatusException.class})
+    public ResponseEntity<Object> handleVerificationException(RuntimeException exception, WebRequest request) {
+        return handleExceptionInternal(exception, exception.getMessage(),
+                new HttpHeaders(), HttpStatus.NOT_ACCEPTABLE, request);
+    }
+
+    @ExceptionHandler(value = {VerificationRequestNotFoundException.class})
+    public ResponseEntity<Object> handleVerificationException(VerificationRequestNotFoundException exception, WebRequest request) {
+        return handleExceptionInternal(exception, exception.getMessage(),
+                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
 }
--- verification/adapter-in-web/src/main/java/hexarch/dms/verification/adapter/in/web/RevisionVerificationRestController.java
+++ verification/adapter-in-web/src/main/java/hexarch/dms/verification/adapter/in/web/RevisionVerificationRestController.java
@@ -1,4 +1,22 @@
 package hexarch.dms.verification.adapter.in.web;
 
+import hexarch.dms.verification.application.port.in.AcceptRevisionUseCase;
+import hexarch.dms.verification.domain.DocumentRevisionId;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
 class RevisionVerificationRestController {
+
+    private final AcceptRevisionUseCase acceptRevisionUseCase;
+
+    @PostMapping("/revisions/{revisionId}/accept")
+    public ResponseEntity<Void> createVerificationRequest(@PathVariable Long revisionId) {
+        acceptRevisionUseCase.apply(new AcceptRevisionUseCase.AcceptRevisionCommand(new DocumentRevisionId(revisionId)));
+        return ResponseEntity.ok().build();
+    }
 }
