96543fdad5d8a302271f382367de637397090f60
Maciej Małecki <maciej.malecki@capgemini.com>
2023-06-12 16:55:04 +0200

    04 queryRevisionById adapter web test

--- preparation/application/src/main/java/hexarch/dms/preparation/application/port/RevisionQueryModel.java
+++ preparation/application/src/main/java/hexarch/dms/preparation/application/port/RevisionQueryModel.java
@@ -6,6 +6,10 @@ import hexarch.dms.preparation.domain.RevisionContent;
 public record RevisionQueryModel(
         Long id,
         Long documentId,
-        DocumentTitle documentTitle,
-        RevisionContent revisionContent) {
+        String documentTitle,
+        String content) {
+
+    public RevisionQueryModel(Long id, Long documentId, DocumentTitle documentTitle, RevisionContent revisionContent) {
+        this(id, documentId, documentTitle.getValue(), revisionContent.getValue());
+    }
 }
