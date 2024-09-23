7b6e71aab699efd414421bffe4497b4865fc3de2
Olga Kichewko <external.olga.kichewko@kuehne-nagel.com>
2024-09-23 13:59:30 +0200

    upgrade java 21 and springboot 3

--- verification/adapter-out-db/src/test/java/hexarch/dms/verification/adapter/out/db/FindRevisionVerificationAdapterTest.java
+++ verification/adapter-out-db/src/test/java/hexarch/dms/verification/adapter/out/db/FindRevisionVerificationAdapterTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import javax.annotation.Resource;
+import jakarta.annotation.Resource;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
