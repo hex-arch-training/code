3a04380f8cf3a749d18d2d898f5c348c2020e5d0
Olga Kichewko <olga.kichewko@capgemini.com>
2025-06-16 15:29:40 +0200

    remove impl of UC that are not part of training and fix some spring basic auth filters

--- verification/adapter-in-web/src/main/java/hexarch/dms/verification/adapter/in/web/RevisionVerificationRestController.java
+++ verification/adapter-in-web/src/main/java/hexarch/dms/verification/adapter/in/web/RevisionVerificationRestController.java
@@ -1,12 +1,9 @@
 package hexarch.dms.verification.adapter.in.web;
 
-import hexarch.dms.verification.application.port.in.AcceptRevisionCommand;
-import hexarch.dms.verification.application.port.in.AcceptRevisionUseCase;
 import hexarch.dms.verification.application.port.in.QueryRevisionPreviewUseCase;
 import hexarch.dms.verification.domain.DocumentRevisionId;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,13 +14,11 @@ import org.springframework.web.server.ResponseStatusException;
 @AllArgsConstructor
 public class RevisionVerificationRestController {
 
-    private final AcceptRevisionUseCase acceptRevisionUseCase;
     private final QueryRevisionPreviewUseCase queryRevisionPreviewUseCase;
 
     @PostMapping("/revision/{revisionId}/accept")
-    public ResponseEntity<Void> createVerificationRequest(@PathVariable("revisionId") Long revisionId) {
-        acceptRevisionUseCase.apply(new AcceptRevisionCommand(new DocumentRevisionId(revisionId)));
-        return ResponseEntity.ok().build();
+    public void acceptVerificationRequest(@PathVariable("revisionId") Long revisionId) {
+
     }
 
     @GetMapping("/revision/{revisionId}/preview")
